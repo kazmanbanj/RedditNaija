6b14f852c6a1aa2ce74f78c8fc14acd01be33d1e
Kazeem Banjoko <banjoko10@gmail.com>
2021-04-24 18:25:42 +0100

    remove default password

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -69,7 +69,7 @@ class RegisterController extends Controller
             'name' => $data['name'],
             'email' => $data['email'],
             'username' => $data['username'],
-            'password' => Hash::make($data['password']),
+            'password' => $data['password'],
         ]);
     }
 }
